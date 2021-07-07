66b08f96a0c8ecd72733cb0a75bdf329da804dbd
onlylosersleftalive <malaniko888@mail.ru>
2021-07-08 00:02:57 +0900

    add state to input and set child-to-parent communication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 
+import * as isVegan from "is-vegan";
 import Form from "./components/Form";
 
 function App() {
+  let saveFormDataHandler = (enteredFormData) => {
+    const formData = {
+      ...enteredFormData,
+    };
+    console.log(formData);
+  };
+
   return (
     <div>
       <div className="main">
         <h1 className="title">Check if your food ingridients are vegan</h1>
-        <Form />
+        <Form onSaveFormData={saveFormDataHandler} />
       </div>
       <div className="footer">
         <p>Made by Sveta M.</p>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,27 @@
 import "./Form.css";
+import React, { useState } from "react";
 
 function Form(props) {
+  const [userInput, setUserInput] = useState("");
+
+  let inputChangeHandler = (event) => {
+    event.preventDefault();
+    setUserInput(event.target.value);
+  };
+
+  let submitHandler = (event) => {
+    event.preventDefault();
+    const enteredData = {
+      ingridient: userInput,
+    };
+    props.onSaveFormData(enteredData);
+    setUserInput("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label>Add one or more ingridients:</label>
-      <input type="text" name="ingridient" />
+      <input type="text" onChange={inputChangeHandler} value={userInput} />
       <button type="submit">Submit</button>
     </form>
   );
